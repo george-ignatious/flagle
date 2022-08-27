fcebb799ae052b47b327b32d83108f004c3e342b
george-ignatious <jojualiasgeorge@gmail.com>
2022-08-27 15:31:23 +0530

    Fix

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -60,7 +60,7 @@ export function Share({
       })
       .join("\n");
 
-    return [title, guessString, "https://flaglegame.azurewebsites.net/"].join("\n");
+    return [title, guessString, "https://flaglegame.azurewebsites.net/"].join("");
   }, [dayString, guesses, hideImageMode, rotationMode, theme]);
 
   return (
